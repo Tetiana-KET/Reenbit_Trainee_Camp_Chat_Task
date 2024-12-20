7c8d83e08d3abea4a8647cc442b3cfa4f549bc96
Tetiana <99186560+Tetiana-KET@users.noreply.github.com>
2024-12-20 16:22:18 +0200

    feat: add static path

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 import { BASE_URL } from '../consts/baseUrl';
 
 export const axiosInstance = axios.create({
-	baseURL: BASE_URL,
+	baseURL: import.meta.env.MODE === 'development' ? BASE_URL : '/api',
 	withCredentials: true,
 });
